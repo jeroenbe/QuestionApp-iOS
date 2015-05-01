b147ee21d8c524ef6ed9159b4648ef2eb14b85f0
Jeroen <jeroen.berrevoets@gmail.com>
2015-05-01 20:52:28 +0200

    Initial Commit

--- QuestionApp/AppDelegate.swift
+++ QuestionApp/AppDelegate.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import CoreData
+import Meteor
+
+let Meteor = METCoreDataDDPClient(serverURL: NSURL(string: "wss://questionapp.meteor.com/websocket"));
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Meteor.connect()
         return true
     }
 
--- QuestionApp/FirstViewController.swift
+++ QuestionApp/FirstViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
+import Meteor
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var subscriptionLoader = SubscriptionLoader();
+        
+        if Meteor.connected {
+            let question: AnyObject! = Meteor.callMethodWithName("getUnreadQuestion", parameters: []){
+                
+            }();
+            subscriptionLoader.addSubscriptionWithName(name: "questionByID", parameters: );
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
