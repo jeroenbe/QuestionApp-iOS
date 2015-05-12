79c710e52e66c818a2d2a890cf485e78721edfd4
Jeroen <jeroen.berrevoets@gmail.com>
2015-05-12 22:05:31 +0200

    made answerCounts functional

--- QuestionApp/ChartViewController.swift
+++ QuestionApp/ChartViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 
 class ChartViewController: UIViewController {
     @IBOutlet weak var yesWidth: NSLayoutConstraint!
-    
     @IBOutlet weak var noWidth: NSLayoutConstraint!
 
+    @IBOutlet weak var yesCountLabel: UILabel!
+    @IBOutlet weak var noCountLabel: UILabel!
+    @IBOutlet weak var skippedCountLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
     func generateChart(yes: Double, no: Double, skipped: Double){
+        self.updateAnswerCount(yes, no: no, skipped: skipped)
+        
         var total = yes + no + skipped
         var newYesWidth = self.yesWidth.withMultiplier(CGFloat(yes/total))
         var newNoWidth = self.noWidth.withMultiplier(CGFloat(no/total))
@@ -26,9 +32,7 @@ class ChartViewController: UIViewController {
         
         self.view.layoutIfNeeded()
         UIView.animateWithDuration(0.45){
-            println(self.yesWidth)
             self.view.removeConstraints([self.yesWidth, self.noWidth])
-            println(self.yesWidth)
             self.view.addConstraints([newYesWidth, newNoWidth])
             (self.yesWidth, self.noWidth) = (newYesWidth, newNoWidth)
             self.view.layoutIfNeeded()
@@ -41,13 +45,25 @@ class ChartViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func enableGraph(){
-        self.view.hidden = false
-         
-    }
     
+    func updateAnswerCount(yes: Double, no: Double, skipped: Double){
+        self.yesCountLabel.text = "\(Int(yes))"
+        println("\(Int(yes))")
+        self.noCountLabel.text = "\(Int(no))"
+        self.skippedCountLabel.text = "\(Int(skipped))"
+    }
+    func resetCounts(){
+        self.yesCountLabel.text = "0"
+        self.noCountLabel.text = "0"
+        self.skippedCountLabel.text = "0"
+    }
+    func enableGraph(){
+        
+        self.view.hidden = false
+    }
     func disableGraph(){
         self.view.hidden = true
+        self.resetCounts()
     }
 }
 
