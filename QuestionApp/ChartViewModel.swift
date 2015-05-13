3f5679cb7a3fbaaf564ae57852013be008d83921
Jeroen <jeroen.berrevoets@gmail.com>
2015-05-13 10:50:35 +0200

    Added ViewModel
    no implementation yet!

---
+++ QuestionApp/ChartViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ChartViewModel.swift
+//  QuestionApp
+//
+//  Created by Jeroen Berrevoets on 13/05/15.
+//  Copyright (c) 2015 Jeroen Berrevoets. All rights reserved.
+//
+
+import Foundation
