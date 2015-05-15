92ee3d396503241681730312dbccbf3c22314777
Jeroen <jeroen.berrevoets@gmail.com>
2015-05-15 13:39:15 +0200

    MVVM refactorstart

---
+++ QuestionApp/Bond+UISegmentedControl.swift
@@ -0,0 +1,9 @@
+//
+//  Bond+UISegmentedControl.swift
+//  QuestionApp
+//
+//  Created by Jeroen Berrevoets on 13/05/15.
+//  Copyright (c) 2015 Jeroen Berrevoets. All rights reserved.
+//
+
+import Foundation
